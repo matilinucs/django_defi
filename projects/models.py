5e2e250c2a18de32924e36883a9c1da0d4054ffe
matilinucs <materodriguez200011@gmail.com>
2023-05-14 13:44:44 -0400

    listo

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre_producto = models.CharField(max_length=200)
     descripcion = models.TextField()
     categoria = models.CharField(max_length=200)
     precio = models.IntegerField()
