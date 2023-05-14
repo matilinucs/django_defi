e46e519645b482abc439360c3bdf20f1707ae182
matilinucs <materodriguez200011@gmail.com>
2023-05-14 11:27:41 -0400

    first-commit

---
+++ projects/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    categoria = models.CharField(max_length=200)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+
+    
